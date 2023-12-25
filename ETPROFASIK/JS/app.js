aa0ee64f15e72a4ce552bda5a46c97b41bd83395
sappawrdn <sapaasapa8@gmail.com>
2023-12-25 21:00:30 +0700

    Finish

---
+++ ETPROFASIK/JS/app.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".slide-container",{
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    centerSlide: true,
+    grabCursor: true,
+    fade: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
